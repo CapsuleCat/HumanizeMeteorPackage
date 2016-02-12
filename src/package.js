255108cfc1336d14ed07af789c6e641d472e7274
Ivan Montiel <idmontie@asu.edu>
2016-02-12 11:16:16 -0700

    Update package.js

--- src/package.js
+++ src/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Humanize values to make them readable for regular people',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/idmontie/meteor-humanize',
+  git: 'https://github.com/CapsuleCat/HumanizeMeteorPackage',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: '../README.md'
